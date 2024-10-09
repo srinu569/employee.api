35477b9143e3c0d8818647ed8d41e7d572e966b9
AmpoluSrinivasarao <srinivas.ampolu@vizaglabs.in>
2024-10-09 18:24:53 +0530

    update

--- src/main/java/com/imaginnovate/employee/api/jpa/repositories/MoEmployeeRepository.java
+++ src/main/java/com/imaginnovate/employee/api/jpa/repositories/MoEmployeeRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.imaginnovate.employee.jpa.entity.MoEmployee;
 
 public interface MoEmployeeRepository extends JpaRepository<MoEmployee, Long> {
-    boolean existsByEmail(String email);
 }
